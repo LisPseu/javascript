7231e1978ede59f9b7fdb6cb1a1c6541d078ef3e
Alesia Bykhovtsova <hamuraptra24@gmail.ru>
2026-01-23 21:04:21 +0300

    домашнее задание, урок 3.

--- scriptLessonThree.js
+++
@@ -1,8 +0,0 @@
-let price = 100;
-price = 150;
-console.log(price)// let - переменная , которой можно перезаписать.
-
-const priceTwo = 100;
-priceTwo = 150;
-console.log(priceTwo)//TypeError: Assignment to constant variable. Ошибка, т.к. контанте нельзя изменить уже заданное значение. const - постоянная переменная.
-
